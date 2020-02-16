66c97d578be5f711051b67c1926464485792f587
Daniel López <d.lopez.1740@gmail.com>
2020-02-16 12:45:28 -0300

    Primera imagen subida

--- src/app/components/photo-form/photo-form.component.ts
+++ src/app/components/photo-form/photo-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../../services/photo.service';
+import { log } from 'util';
+
+interface HtmlInputEvent extends Event {
+  target: HTMLInputElement & EventTarget;
+}
 
 @Component({
   selector: 'app-photo-form',
@@ -6,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-form.component.css']
 })
 export class PhotoFormComponent implements OnInit {
+  file: File;
+  photoSelected: string | ArrayBuffer;
 
-  constructor() { }
+  constructor(private photoService: PhotoService) {
+  }
 
   ngOnInit(): void {
   }
 
+  onPhotoSelected(event: HtmlInputEvent): void {
+    if (event.target.files && event.target.files[0]) {
+      this.file = event.target.files[0];
+      // Image Preview
+      const reader = new FileReader();
+      reader.onload = _ => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+  uploadPhoto(title: HTMLInputElement, description: HTMLTextAreaElement): boolean {
+    this.photoService
+        .createPhoto(title.value, description.value, this.file)
+        .subscribe(res => console.log(res), err => console.error(err));
+
+    return false;
+  }
 }
--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  private url = 'http://localhost:4000/api';
+
+  constructor(private http: HttpClient) { }
+
+  createPhoto(title: string, description: string, photo: File) {
+    const fd = new FormData();
+    fd.append('title', title);
+    fd.append('description', description);
+    fd.append('image', photo);
+
+    return this.http.post(`${this.url}/photo`, fd);
+  }
 }
